6f9900e9ed865e8f723d05d88ed9e6a92f970efe
Johannes Spies <13813209+neftlon@users.noreply.github.com>
2024-02-22 00:50:21 +0100

    shamelessly copy merge and shuffle generating code for GPT-4 from Andrej Karpathy

--- python/minibpe/__init__.py
+++ python/minibpe/__init__.py
@@ -1,6 +1,8 @@
-from minibpe._native import NativeBasicTokenizer
-from minibpe.basic import PyBasicTokenizer
+from minibpe._native import NativeBasicTokenizer, NativeGpt4Tokenizer
 from minibpe._native import py_tokenizer_with_presplit as tokenizer_with_presplit
+from minibpe.basic import PyBasicTokenizer
+
 BasicTokenizer = NativeBasicTokenizer
+Gpt4Tokenizer = NativeGpt4Tokenizer
 
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
